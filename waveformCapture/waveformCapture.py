2a2cc55e56b2384dd7699c3cf7b85436c5882897
PaulSchubert <paul.frederik.schubert@gmail.com>
2023-02-15 07:17:00 -0800

    base functions

--- waveformCapture/waveformCapture.py
+++ waveformCapture/waveformCapture.py
@@ -16,7 +16,7 @@ class app(tk.Frame):
 
         # textbox for save path entry & button to set it
         self.file_path_entry = tk.Text(height=1, width=30, bg='lightblue')
-        self.file_path_entry.insert(tk.END, '~/src/testingData/')
+        self.file_path_entry.insert(tk.END, '../../testingData/')
         self.file_path_entry.grid( row=3, column=1)
 
         self.start_button = tk.Button(text="Start", command=self.start_app)
@@ -26,33 +26,41 @@ class app(tk.Frame):
         self.last_updated.grid(row=7, column=3)
 
 
-    def start_app(self):
+    def save_paths(self):
+        # getting the initially defined paths
         self.url = self.url_entry.get('1.0', 'end-1c')
         self.save_path = self.file_path_entry.get('1.0', 'end-1c')
 
-        self.open_save_file()
-        self.get_and_save()
+    def start_app(self):
 
+        # keep save paths for file & url
+        self.save_paths()
+        self.curr_save_path = self.save_path + 'test01'
 
-        # THIS REPEAT ( every 5 min) IS NOT WORKING YET
-        #root.after(300000, self.get_and_save)
-        root.process.after(1, self.get_and_save)
+        file_state = self.open_save_file()
+        if file_state == True:
+            self.get_and_save()
+            self.app_loop()
+            
+
+    def app_loop(self):
+        # will make request and save to next file every 5 min
+        
+        pass;
 
 
     def get_and_save(self):
         # calls the required functions and updates the 'last updated' label
-        # do stuff
-
+        self.data = "this will be saved"
         
-        #zipped = get_raw_data()
+        #zipped = self.get_raw_data()
+        #self.get_raw_data()
 
-        # send data to be saved to this function
-        #save_data()
+        self.save_data()
 
+        # update the 'last updated' label
         current_time = dt.datetime.now().strftime("%H:%M:%S")
         self.last_updated.config(text="Last updated: " + current_time)
-
-
 
 
     def get_raw_data(self):
@@ -73,24 +81,29 @@ class app(tk.Frame):
         t = [1.0e6 * (float(wfmpre.split(';')[8]) * float(i) + float(wfmpre.split(';')[10])) for i in range(0, len(wfm))]
         volt = [1.0e3 * (((dl / 256) - float(wfmpre.split(';')[14])) * float(wfmpre.split(';')[12]) - float(wfmpre.split(';')[13])) for dl in wfm]
 
+        #self.data = zip(t, volt)
+        #self.save_data()
         return zip(t, volt)
 
 
-    def save_data(self, zipped):
+    def save_data(self):
         # a function to save the zipped data into the open file
-        pass;
+        print(self.data)
+        self.saveFile.write(self.data)
 
     def open_save_file(self):
         # saves the recently received data to new file
-        print(self.save_path)
+        print(self.curr_save_path)
 
         # this needs to be double checked
         try:
-            saveFile = open(self.save_path, 'w')
+            self.saveFile = open(self.curr_save_path, 'w')
             # now things can happen to the file
             print("File opened successfully")
+            return True
         except:
             print("Failed to open the file")
+            return False
 
         # needs to be done at some point
         # saveFile.close()
