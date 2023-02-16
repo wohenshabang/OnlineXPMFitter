24ff0dde7f9dfd5456773c16c54e6ef904ec3e02
PaulSchubert <paul.frederik.schubert@gmail.com>
2023-02-15 16:55:29 -0800

    getting and saving data

--- waveformCapture/waveformCapture.py
+++ waveformCapture/waveformCapture.py
@@ -1,7 +1,11 @@
 import tkinter as tk
-import requests
+import urllib.request
 import time
 import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+import numpy as np
 
 
 class app(tk.Frame):
@@ -26,6 +30,18 @@ class app(tk.Frame):
         self.last_updated = tk.Label(text="Last updated: N/A")
         self.last_updated.grid(row=7, column=3)
 
+        # for the graph
+        self.parent = parent
+        self.figure1 = Figure(figsize=(6, 5), dpi=100)
+        self.plt1 = self.figure1.add_subplot(111)
+        self.canvas1 = FigureCanvasTkAgg(self.figure1, master=self.parent)
+        self.plot_widget1 = self.canvas1.get_tk_widget()
+        self.plot_widget1.grid(row=3, column=5)
+
+
+        self.counter = 0
+
+
 
     def save_paths(self):
         # getting the initially defined paths
@@ -37,6 +53,15 @@ class app(tk.Frame):
         # keep save paths for file & url
         self.save_paths()
 
+        #self.curr_save_path = self.save_path + 'test' + str(self.counter).zfill(3)
+
+        self.get_and_save()
+
+
+
+    def grab(self):
+        pass
+        '''
         i = 1
         while True:
             self.curr_save_path = self.save_path + 'test' + str(i).zfill(3)
@@ -47,41 +72,65 @@ class app(tk.Frame):
                 i += 1
                 # we probably don't want to use sleep... use scheduler?
                 time.sleep(5)
+        '''
+
 
     def get_and_save(self):
         # calls the required functions and updates the 'last updated' label
-        self.data = "this will be saved"
+
         
         #zipped = self.get_raw_data()
-        #self.get_raw_data()
+        self.get_raw_data()
 
-        self.save_data()
+        #self.save_data()
 
         # update the 'last updated' label
         current_time = dt.datetime.now().strftime("%H:%M:%S")
         self.last_updated.config(text="Last updated: " + current_time)
 
 
+
     def get_raw_data(self):
         # makes requests and processes data into floats
         # this request is having difficulties, check with Kolo
 
+        self.curr_save_path = self.save_path + 'test' + str(self.counter).zfill(3)
+
         data = ''
-        f = requests.get(self.url + '/?COMMAND=curve?')
+        f = urllib.request.urlopen(self.url + '/?COMMAND=curve?')
+        #f = requests.get(self.url + '/?COMMAND=curve?')
         data = f.read().decode()
         print('received ' + data)
 
         wfm = [float(u) for u in data.split(',')]
 
         # calling wfmpre to convert wfm to MS and VOLTS
-        f2 = requests.get(self.url + '/?COMMAND=wfmpre?')
+        f2 = urllib.request.urlopen(self.url + '/?COMMAND=wfmpre?')
+        #f2 = requests.get(self.url + '/?COMMAND=wfmpre?')
         wfmpre = f2.read().decode()
 
         # conversions to usable values
         t = [1.0e6 * (float(wfmpre.split(';')[8]) * float(i) + float(wfmpre.split(';')[10])) for i in range(0, len(wfm))]
-        volt = [1.0e3 * (((dl / 256) - float(wfmpre.split(';')[14])) * float(wfmpre.split(';')[12]) - float(wfmpre.split(';')[13])) for dl in wfm]
+        volt = [1.0e3 * (((float(dl) - float(wfmpre.split(';')[14]))/1.0) * float(wfmpre.split(';')[12]) - float(wfmpre.split(';')[13])) for dl in wfm]
 
-        #self.data = zip(t, volt)
+        self.plt1.cla()
+        self.plt1.plot(t, volt, 'g-')
+        self.plt1.set_title("Most recent waveform")
+        self.plt1.set_ylabel("MilliVolts")
+        self.plt1.set_xlabel(u"Time (\u03bcs)")
+        self.canvas1.draw_idle()
+
+
+        
+
+        data = np.transpose([np.array(t), np.array(volt)])
+
+        np.savetxt(self.curr_save_path, data, delimiter=',')
+
+
+        self.counter += 1
+
+        #self.data = 
         #self.save_data()
         return zip(t, volt)
 
